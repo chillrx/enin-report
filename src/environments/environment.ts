90adb5b33d217ae73600c80ed653f97d031ac37b
Victor Machado <joelvictor2012@gmail.com>
2020-08-24 13:00:43 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,5 @@
 export const environment = {
   production: false,
   urlToApi: "https://events.al.senai.br/api/",
-  //urlToApi: "http://10.83.3.191/api/",
   urlToOauthToken: "https://events.al.senai.br/oauth/token"
 };
